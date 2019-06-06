8217f3e0de845b595c6477bd3241cf607d16bc74
t-tullis <t.tullis34@gmail.com>
2019-06-06 14:48:30 -0700

    completed eating_cookies and rps algorithms

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,16 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  rps = ['rock', 'paper', 'scissors']
+  outcomes = []
 
+  def rps_results(roundsLeft, play):
+    if roundsLeft == 0:
+      return outcomes.append(play)
+    for i in rps:
+      rps_results(roundsLeft- 1, play + [i])
+  rps_results(n, [])
+  return outcomes
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -12,3 +20,5 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+
+  print(rock_paper_scissors(2))
